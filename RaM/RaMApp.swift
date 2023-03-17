8d2cc2e5032a1e7e5f69c783757c7027ea0d82d4
FireDelic <35647091+FireDelic@users.noreply.github.com>
2023-03-17 10:56:26 +0100

    Initial Commit

---
+++ RaM/RaMApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaMApp.swift
+//  RaM
+//
+//  Created by Fahir Delic on 17.03.23.
+//
+
+import SwiftUI
+
+@main
+struct RaMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
